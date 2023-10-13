ef2c055e3a783663008dabe7c8e4b7ecf1becf55
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-10-13 13:00:48 +0600

    Add project files.

---
+++ Mod_5_1(Dom_z)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Mod_5_1_Dom_z_
+{
+
+    public class WebRequestHandler
+    {
+        public void MakeWebRequest(string url)
+        {
+            try
+            {
+                throw new Exception("Web resource not found.");
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("Web resource not found."))
+                {
+                    Console.WriteLine("Error: The requested web resource does not exist.");
+                }
+                else
+                {
+
+                }
+
+                Environment.Exit(0);
+            }
+        }
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webRequestHandler = new WebRequestHandler();
+            webRequestHandler.MakeWebRequest("https://www.primer.com");
+        }
+    }
+}
